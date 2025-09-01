7d9328564ca3ace9259f5f3a5355aa96c18f76b8
mlight lee <mlight.lee@outlook.com>
2025-09-01 22:19:00 +0800

    feat: remove uv and normal attribute data from TextGeometry to reduce memory cost (#25)

--- src/font/meshTextShape.ts
+++ src/font/meshTextShape.ts
@@ -1,8 +1,12 @@
 import * as THREE from 'three';
+import { mergeVertices } from 'three/examples/jsm/utils/BufferGeometryUtils.js';
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
 
 import { BaseTextShape } from './baseTextShape';
 import { MeshFont } from './meshFont';
+import { NormalComputationToggle } from './normalComputationToggle';
+
+const _tmp = new THREE.Vector2();
 
 /**
  * Represents a text shape for mesh-based fonts (e.g., TTF, OTF, WOFF).
@@ -32,12 +36,29 @@ export class MeshTextShape extends BaseTextShape {
    * @returns A THREE.js BufferGeometry representing the text shape
    */
   toGeometry(): THREE.BufferGeometry {
-    return new TextGeometry(this.char, {
-      font: this.font.font,
-      depth: 0,
-      size: this.fontSize,
-      curveSegments: 3, // change this to increase/decrease display precision
-      bevelSegments: 3,
+    // Use NormalComputationToggle to disable generating 'normal' data in returned geometry
+    // to save computation cost because rendering font characters don't need it.
+    return NormalComputationToggle.runWithoutNormals<THREE.BufferGeometry>(() => {
+      const geometry = new TextGeometry(this.char, {
+        font: this.font.font,
+        depth: 0,
+        size: this.fontSize,
+        curveSegments: 3, // change this to increase/decrease display precision
+        bevelSegments: 3,
+        // Pass dummy uv generator to save computation cost because rendering font characters
+        // always use color material and don't need 'uv' data.
+        UVGenerator: {
+          generateTopUV: () => [_tmp, _tmp, _tmp],
+          generateSideWallUV: () => [_tmp, _tmp, _tmp, _tmp],
+        },
+      });
+      if (geometry.hasAttribute('uv')) {
+        geometry.deleteAttribute('uv');
+      }
+      if (geometry.hasAttribute('normal')) {
+        geometry.deleteAttribute('normal');
+      }
+      return mergeVertices(geometry, 1e-6);
     });
   }
 
---
+++ src/font/normalComputationToggle.ts
@@ -0,0 +1,76 @@
+import { BufferGeometry } from 'three';
+
+/**
+ * Utility class for temporarily disabling `BufferGeometry.computeVertexNormals`.
+ *
+ * By default, Three.js computes vertex normals for many geometries,
+ * which can be unnecessary (e.g., when using `MeshBasicMaterial` that does not require lighting).
+ * This class lets you temporarily replace `computeVertexNormals` with a no-op
+ * to save CPU time during geometry creation.
+ *
+ * Example:
+ * ```ts
+ * import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
+ * import { NormalComputationToggle } from "./NormalComputationToggle";
+ *
+ * // Create geometry without computing normals
+ * const textGeom = NormalComputationToggle.runWithoutNormals(() =>
+ *   new TextGeometry("Fast!", { font, size: 1, height: 0.2 })
+ * );
+ * ```
+ */
+export class NormalComputationToggle {
+  /**
+   * Stores the original `computeVertexNormals` method from BufferGeometry.
+   */
+  private static originalComputeVertexNormals = BufferGeometry.prototype.computeVertexNormals;
+
+  /**
+   * Dummy replacement for `computeVertexNormals` that does nothing.
+   */
+  private static dummyComputeVertexNormals(): void {
+    // Intentionally left blank
+  }
+
+  /**
+   * Disable vertex normal computation globally.
+   *
+   * After calling this, all calls to `computeVertexNormals`
+   * will do nothing until {@link restore} is called.
+   */
+  static disable(): void {
+    BufferGeometry.prototype.computeVertexNormals = this.dummyComputeVertexNormals;
+  }
+
+  /**
+   * Restore the original `computeVertexNormals` implementation.
+   */
+  static restore(): void {
+    BufferGeometry.prototype.computeVertexNormals = this.originalComputeVertexNormals;
+  }
+
+  /**
+   * Execute a function with normal computation disabled,
+   * then automatically restore afterwards.
+   *
+   * This is the safest way to create geometries without normals.
+   *
+   * Example:
+   * ```ts
+   * const geom = NormalComputationToggle.runWithoutNormals(() =>
+   *   new TextGeometry("World", { font, size: 1, height: 0.2 })
+   * );
+   * ```
+   *
+   * @param fn - A callback that creates the geometry.
+   * @returns The result of the callback, e.g., a geometry.
+   */
+  static runWithoutNormals<T>(fn: () => T): T {
+    this.disable();
+    try {
+      return fn();
+    } finally {
+      this.restore();
+    }
+  }
+}
